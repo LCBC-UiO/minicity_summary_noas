3049ab81c09fd21af938a29c55ff44d4a5220180
Monokkel <knuteiliv@gmail.com>
2023-02-03 12:44:58 +0100

    Logging mini city version used

--- scripts/minicity_generate_noas_data.R
+++ scripts/minicity_generate_noas_data.R
@@ -14,7 +14,7 @@ last_id_wo_ctrl <- 62
 
 # Initialize dataframe to be used for results
 minidata <- tribble(
-  ~subject_id, ~project_id, ~wave_code, ~target_hits, ~dfp_mean, ~dfp_median, ~dfp_std
+  ~subject_id, ~project_id, ~wave_code, ~target_hits, ~dfp_mean, ~dfp_median, ~dfp_std, ~mini_version
 )
 
 # Loop through all event logs
@@ -72,6 +72,10 @@ for (log in 1: length(logs))
     long_id <- log_id
     print(paste("WARNING: Incorrect ID format ",log_id," for log ", logs[log]))
   }
+    
+  # Log which version of MiniCity was used for this wave
+  version_index <- tail(unlist(gregexpr('i', logs[log])), n=1)+1
+  version <- substr(logs[log],version_index,version_index)
   
   subject_id <- add_check_digit(long_id)
   has_ctrl <- FALSE
@@ -87,7 +91,8 @@ for (log in 1: length(logs))
   # Add current log's data as a row to the output dataframe
   minidata <- minidata %>% add_row(subject_id = subject_id, project_id = "S2C",wave_code = wave,
                                    target_hits = current_log_targets, dfp_mean = current_overshoot_mean[[1]],
-                                   dfp_median = current_overshoot_median[[1]],dfp_std = current_overshoot_sd[[1]])
+                                   dfp_median = current_overshoot_median[[1]],dfp_std = current_overshoot_sd[[1]],
+                                   mini_version = version)
 }
 
 # Write data to results
